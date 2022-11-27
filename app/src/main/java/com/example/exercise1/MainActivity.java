83bcdd3d2efbc2b3072db4c3c4a249a58885d72d
Sonia364 <c0872364@mylambton.ca>
2022-11-27 18:00:04 +0000

    Implemented on click method to calculate BMI

--- app/src/main/java/com/example/exercise1/MainActivity.java
+++ app/src/main/java/com/example/exercise1/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.exercise1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText height = findViewById(R.id.height);
+        EditText weight = findViewById(R.id.weight);
+        Button  calculateBtn = findViewById(R.id.calculate);
+
+        calculateBtn.setOnClickListener(
+                view -> {
+                    double heightVal = Double.parseDouble(height.getText().toString());
+                    double weightVal = Double.parseDouble(weight.getText().toString());
+                    double BMIValue  =  weightVal / (heightVal * heightVal);
+                    Toast.makeText(this, "The Body Mass Index (BMI) is " + String.format("%.2f", BMIValue), Toast.LENGTH_LONG).show();
+                }
+        );
+
     }
+
 }
